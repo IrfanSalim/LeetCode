32875cf92f612da0f55eb74f4c8098dde5fa1018
Irfan <irfansalim77@gmail.com>
2024-01-04 14:21:47 +0530

    Add ?? 876.middle-of-the-linked-list.py

---
+++ 876.middle-of-the-linked-list.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=876 lang=python
+#
+# [876] Middle of the Linked List
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return head
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+        
+# @lc code=end
+
