9643a904f345940b8dfcca7e87a722ebdd1a7321
Irfan <irfansalim77@gmail.com>
2024-01-31 19:36:39 +0530

    Add  M 14.longest-common-prefix.py

--- 14.longest-common-prefix.py
+++ 14.longest-common-prefix.py
@@ -11,19 +11,30 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-        prev = strs[0]
-        pf = len(strs[0])
-        for i in range(1, len(strs)):
-            n = len(strs[i])
-            if n == 0:
-                return ""
-            if n < pf:
-                pf = n
-            while pf and prev[:pf] != strs[i][:pf]:
-                pf -= 1
-            if pf == 0:
-                return ""
-            prev = strs[i]
-        return prev[:pf]
+
+        if not strs:
+            return ''
+
+        for i in range(len(strs[0])):
+            for j in range(1, len(strs)):
+                if i == len(strs[j]) or strs[j][i] != strs[0][i]:
+                    return strs[0][:i]
+
+        return strs[0]
+
+        # prev = strs[0]
+        # pf = len(strs[0])
+        # for i in range(1, len(strs)):
+        #     n = len(strs[i])
+        #     if n == 0:
+        #         return ""
+        #     if n < pf:
+        #         pf = n
+        #     while pf and prev[:pf] != strs[i][:pf]:
+        #         pf -= 1
+        #     if pf == 0:
+        #         return ""
+        #     prev = strs[i]
+        # return prev[:pf]
 # @lc code=end
 
