52600cb149132814ed1ac3a71ed0a1f4f61e7340
Irfan <irfansalim77@gmail.com>
2024-02-15 23:13:32 +0530

    Add ?? 283.move-zeroes.py

---
+++ 283.move-zeroes.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=283 lang=python
+#
+# [283] Move Zeroes
+#
+
+# @lc code=start
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        if not nums:
+            return []
+        i, j = 0, 0
+        while j < len(nums):
+            if nums[j] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1
+                i += 1
+            else:
+                j += 1
+        return nums
+# @lc code=end
+
