7180a87e4187df3a0c5047c3d269c88222c2be02
Irfan <irfansalim77@gmail.com>
2024-02-23 17:08:05 +0530

    Add ?? 450.delete-node-in-a-bst.py

---
+++ 450.delete-node-in-a-bst.py
@@ -0,0 +1,40 @@
+#
+# @lc app=leetcode id=450 lang=python
+#
+# [450] Delete Node in a BST
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def deleteNode(self, root, key):
+        """
+        :type root: TreeNode
+        :type key: int
+        :rtype: TreeNode
+        """
+        if not root:
+            return
+        elif root.val > key:
+            root.left = self.deleteNode(root.left, key)
+        elif root.val < key:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if not root.left and not root.right:
+                return None
+            elif not root.left or not root.right:
+                return root.left if root.left else root.right
+            else:
+                temp = root.left
+                while temp.right:
+                    temp = temp.right
+                root.val = temp.val
+                root.left = self.deleteNode(root.left, temp.val)
+        return root
+# @lc code=end
+
