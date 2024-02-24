69081d60a79fe145accf0f36e3398755f062eac2
Irfan <irfansalim77@gmail.com>
2024-02-24 13:18:56 +0530

    Add  M 543.diameter-of-binary-tree.py

--- 543.diameter-of-binary-tree.py
+++ 543.diameter-of-binary-tree.py
@@ -17,16 +17,29 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
-        dia = [0]
-        def height(A):
+        # approach with a pair return
+        def diameter(A):
             if not A:
-                return -1
-            left = height(A.left)
-            right = height(A.right)
-            dia[0] = max(dia[0], left + right + 2)
-            return max(left, right) + 1
-        height(root)
-        return dia[0]
+                return [-1, 0]
+            left = diameter(A.left)
+            right = diameter(A.right)
+            ht = max(left[0], right[0]) + 1
+            dia = max(left[1], right[1], left[0] + right[0] + 2)
+            return [ht, dia]
+        
+        return diameter(root)[1]
+
+        # approach with a global variable
+        # dia = [0]
+        # def height(A):
+        #     if not A:
+        #         return -1
+        #     left = height(A.left)
+        #     right = height(A.right)
+        #     dia[0] = max(dia[0], left + right + 2)
+        #     return max(left, right) + 1
+        # height(root)
+        # return dia[0]
         
 # @lc code=end
 
