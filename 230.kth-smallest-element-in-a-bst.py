ed6f1f7aad5dbcafe609fcd23075cb51a1f60e8e
Irfan <irfansalim77@gmail.com>
2024-02-25 23:59:11 +0530

    Add  M 230.kth-smallest-element-in-a-bst.py

--- 230.kth-smallest-element-in-a-bst.py
+++ 230.kth-smallest-element-in-a-bst.py
@@ -18,18 +18,42 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        count = [0]
-        ans = [0]
-        def dfs(A, k):
-            if not A:
-                return
-            dfs(A.left, k)
-            count[0] += 1
-            if count[0] == k:
-                ans[0] = A.val
-            dfs(A.right, k)
-        dfs(root, k)
-        return ans[0]
-        
+        #optimal approach with morris inorder traversal
+        #no space used
+        curr = root
+        while curr:
+            if not curr.left:
+                k -= 1
+                if k == 0:
+                    return curr.val
+                curr = curr.right
+            else:
+                temp = curr.left
+                while temp.right and temp.right != curr:
+                    temp = temp.right
+                if not temp.right:
+                    temp.right = curr
+                    curr = curr.left
+                else:
+                    temp.right = None
+                    k -= 1
+                    if k == 0:
+                        return curr.val
+                    curr = curr.right
+
+        #dfs approach to traverse inorder and find kth number
+        #approach will take O(height) space
+        # count = [0]
+        # ans = [0]
+        # def dfs(A, k):
+        #     if not A:
+        #         return
+        #     dfs(A.left, k)
+        #     count[0] += 1
+        #     if count[0] == k:
+        #         ans[0] = A.val
+        #     dfs(A.right, k)
+        # dfs(root, k)
+        # return ans[0]
 # @lc code=end
 
