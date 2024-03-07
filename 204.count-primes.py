633c7f1fbc4e624d2467122b1c1cf14feaf4ac9d
Irfan <irfansalim77@gmail.com>
2024-03-07 20:32:33 +0530

    Add  M 204.count-primes.py

--- 204.count-primes.py
+++ 204.count-primes.py
@@ -11,18 +11,33 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        arr = [True] * (n + 1)
+        if n <= 1:
+            return 0
+        
+        primes = [True] * n
+        primes[0] = primes[1] = False
+        cnt = 1
+
         for i in range(2, int(n**0.5) + 1):
-            if arr[i]:
-                j = i * i
-                while j <= n:
-                    arr[j] = False
-                    j += i
-            i += 1
-        count = 0
-        for i in range(2, n):
-            if arr[i]:
-                count += 1
-        return count
+            if primes[i]:
+                for j in range(i*i, n, i):
+                    if primes[j]:
+                        cnt += 1
+                    primes[j] = False
+
+        return n - cnt - 1
+        # arr = [True] * (n + 1)
+        # for i in range(2, int(n**0.5) + 1):
+        #     if arr[i]:
+        #         j = i * i
+        #         while j <= n:
+        #             arr[j] = False
+        #             j += i
+        #     i += 1
+        # count = 0
+        # for i in range(2, n):
+        #     if arr[i]:
+        #         count += 1
+        # return count
 # @lc code=end
 
