a2283956826c0876100614ad0e2484ee61018c7f
Irfan <irfansalim77@gmail.com>
2024-03-09 21:44:20 +0530

    Add  D "leetcode/editor/en/[]Longest Palindromic Substring.py"  D "leetcode/editor/en/doc/content/[]Longest Palindromic Substring.md" ?? 5.longest-palindromic-substring.py

--- 5.longest-palindromic-substring.py
+++ 5.longest-palindromic-substring.py
@@ -1,33 +1,10 @@
-# Given a string s, return the longest palindromic substring in s. 
-# 
-#  
-#  Example 1: 
-# 
-#  
-# Input: s = "babad"
-# Output: "bab"
-# Explanation: "aba" is also a valid answer.
-#  
-# 
-#  Example 2: 
-# 
-#  
-# Input: s = "cbbd"
-# Output: "bb"
-#  
-# 
-#  
-#  Constraints: 
-# 
-#  
-#  1 <= s.length <= 1000 
-#  s consist of only digits and English letters. 
-#  
-# 
-#  Related Topics String Dynamic Programming 👍 28714 👎 1717
+#
+# @lc app=leetcode id=5 lang=python
+#
+# [5] Longest Palindromic Substring
+#
 
-
-# leetcode submit region begin(Prohibit modification and deletion)
+# @lc code=start
 class Solution(object):
     def longestPalindrome(self, s):
         """
@@ -61,5 +38,5 @@ class Solution(object):
             p1 -= 1
             p2 += 1
         return [p2 - p1 + 1, p1, p2]
-        
-# leetcode submit region end(Prohibit modification and deletion)
+# @lc code=end
+
