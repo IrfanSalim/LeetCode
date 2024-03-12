9ff6a78fffa812a9c89081674177d27e18f5aefc
Irfan <irfansalim77@gmail.com>
2024-03-12 17:36:55 +0530

    Add  M 378.kth-smallest-element-in-a-sorted-matrix.py

--- 378.kth-smallest-element-in-a-sorted-matrix.py
+++ 378.kth-smallest-element-in-a-sorted-matrix.py
@@ -13,15 +13,30 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        # using Min heap
         heap = []
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if len(heap) < k:
-                    heapq.heappush(heap, -matrix[i][j])
-                else:
-                    if matrix[i][j] < -heap[0]:
-                        heapq.heappop(heap)
-                        heapq.heappush(heap, -matrix[i][j])
-        return -heap[0]
+        m, n = len(matrix), len(matrix[0])
+        for row in range(min(k, m)):
+            heapq.heappush(heap, (matrix[row][0], row, 0))
+        
+        for i in range(k):
+            val, row, column = heapq.heappop(heap)
+            if column + 1 < n:
+                heapq.heappush(heap, (matrix[row][column+1], row, column+1))
+
+        return val
+
+
+        # using Max heap
+        # heap = []
+        # for i in range(len(matrix)):
+        #     for j in range(len(matrix[0])):
+        #         if len(heap) < k:
+        #             heapq.heappush(heap, -matrix[i][j])
+        #         else:
+        #             if matrix[i][j] < -heap[0]:
+        #                 heapq.heappop(heap)
+        #                 heapq.heappush(heap, -matrix[i][j])
+        # return -heap[0]
 # @lc code=end
 
