f393387e66f987a146996f90ce9f609ce333530c
Irfan <irfansalim77@gmail.com>
2024-03-12 20:38:42 +0530

    Add  M 378.kth-smallest-element-in-a-sorted-matrix.py

--- 378.kth-smallest-element-in-a-sorted-matrix.py
+++ 378.kth-smallest-element-in-a-sorted-matrix.py
@@ -13,20 +13,50 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        # using Min heap
-        heap = []
+        # Binary search approach
         m, n = len(matrix), len(matrix[0])
-        for row in range(min(k, m)):
-            heapq.heappush(heap, (matrix[row][0], row, 0))
+
+        def countLessOrEqual(num):
+            count = 0
+            for i in range(m):
+                if matrix[i][0] > num:
+                    break
+                for j in range(n):
+                    if matrix[i][j] <= num:
+                        count += 1
+                    else:
+                        break
+            return count
         
-        for i in range(k):
-            val, row, column = heapq.heappop(heap)
-            if column + 1 < n:
-                heapq.heappush(heap, (matrix[row][column+1], row, column+1))
+        left, right = matrix[0][0], matrix[m-1][n-1]
+        ans = -1
+        while left <= right:
+            mid = left + (right - left)//2
 
-        return val
+            if countLessOrEqual(mid) >= k:
+                ans = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+        return ans
 
+        #--------------------------------------------------------------------------------------------------------
 
+        # using Min heap
+        # heap = []
+        # m, n = len(matrix), len(matrix[0])
+        # for row in range(min(k, m)):
+        #     heapq.heappush(heap, (matrix[row][0], row, 0))
+        
+        # for i in range(k):
+        #     val, row, column = heapq.heappop(heap)
+        #     if column + 1 < n:
+        #         heapq.heappush(heap, (matrix[row][column+1], row, column+1))
+
+        # return val
+
+        # -------------------------------------------------------------------------------------------------------
+        
         # using Max heap
         # heap = []
         # for i in range(len(matrix)):
