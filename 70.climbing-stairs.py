5403d2f1606c3c72bc82d3ef17b9ab5492efff5f
Irfan <irfansalim77@gmail.com>
2024-03-16 13:42:50 +0530

    Add  M 70.climbing-stairs.py ?? 176.second-highest-salary.py

--- 70.climbing-stairs.py
+++ 70.climbing-stairs.py
@@ -17,6 +17,44 @@ class Solution(object):
             prev = curr
             curr = next
         return curr
+
+
+        #using top down DP, or tabulation
+        # if n <= 2:
+        #     return n
+        
+        # dp = [1, 2]
+        
+        # for i in range(2, n):
+        #     curr = dp[i-1] + dp[i-2]
+        #     dp.append(curr)
+        
+        # return dp[-1]
+
+
+        # using DP bottom-up
+        # dp = [-1] * (n+1)
+
+        # def rec(A):
+        #     if A <= 2:
+        #         return A
+            
+        #     if dp[A] != -1:
+        #         return dp[A]
+            
+        #     dp[A] = rec(A-1) + rec(A-2)
+        #     return dp[A]
+        
+        # return rec(n)
+
+
+        #using basic recursion
+        # def rec(A):
+        #     if A <= 2:
+        #         return A
+        #     return rec(A - 1) + rec(A-2)
+
+        # return rec(n)
         
 # @lc code=end
 
