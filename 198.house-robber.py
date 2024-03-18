3d122c6c13d6a5fa319e714d12c46ae61e93c3ba
Irfan <irfansalim77@gmail.com>
2024-03-18 18:16:36 +0530

    Add  M 198.house-robber.py

--- 198.house-robber.py
+++ 198.house-robber.py
@@ -11,16 +11,24 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        dp = [-1] * (len(nums) + 1)
+        odd, eve = 0, 0
+        for i in range(len(nums)):
+            temp = max(odd + nums[i], eve)
+            odd = eve
+            eve = temp
+        return eve
 
-        def getMax(i, A):
-            if i >= len(A):
-                return 0
-            if dp[i] != -1:
-                return dp[i]
-            dp[i] = max(A[i] + getMax(i+2, A), getMax(i+1, A))
-            return dp[i]
+        # dp solution with recursion
+        # dp = [-1] * (len(nums) + 1)
+
+        # def getMax(i, A):
+        #     if i >= len(A):
+        #         return 0
+        #     if dp[i] != -1:
+        #         return dp[i]
+        #     dp[i] = max(A[i] + getMax(i+2, A), getMax(i+1, A))
+        #     return dp[i]
         
-        return getMax(0, nums)
+        # return getMax(0, nums)
 # @lc code=end
 
