bee2e5ecfd0cefb24bd337c82be3331a61616ac9
Irfan <irfansalim77@gmail.com>
2024-03-22 00:07:12 +0530

    Add  M 62.unique-paths.py

--- 62.unique-paths.py
+++ 62.unique-paths.py
@@ -12,16 +12,27 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        dp = [[-1 for i in range(n)] for j in range(m)]
+        dp = [1 for i in range(n)]
+
+        for i in range(m-1):
+            for j in range(1, n):
+                dp[j] = dp[j] + dp[j-1]
         
-        for i in range(m):
-            for j in range(n):
-                if i == 0 or j == 0:
-                    dp[i][j] = 1
-                else:
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[-1]
+
+        #------------------------------------------------------------------------------------------------------
+
+        # DP with bottom up approach
+        # dp = [[-1 for i in range(n)] for j in range(m)]
         
-        return dp[m-1][n-1]
+        # for i in range(m):
+        #     for j in range(n):
+        #         if i == 0 or j == 0:
+        #             dp[i][j] = 1
+        #         else:
+        #             dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        
+        # return dp[m-1][n-1]
 
         #------------------------------------------------------------------------------------------------------
 
