72b75026ff4cd145820e655812e0f3444b736d2b
Irfan <irfansalim77@gmail.com>
2024-03-21 22:58:06 +0530

    Add  M 174.dungeon-game.py

--- 174.dungeon-game.py
+++ 174.dungeon-game.py
@@ -14,19 +14,34 @@ class Solution(object):
         A = dungeon
         m, n = len(A), len(A[0])
 
-        # DP with top down approach
-        dp = [[-1 for i in range(n)] for j in range(m)]
-
-        def health(A, i, j):
-            if i >= m or j >= n:
-                return float('inf')
-            if i == m-1 and j == n-1:
-                return max(1, 1 - A[i][j])
-            if dp[i][j] != -1:
-                return dp[i][j]
-            dp[i][j] = max(1, min(health(A, i+1, j), health(A, i, j+1)) - A[i][j])
-            return dp[i][j]
+        for i in range(m-1, -1, -1):
+            for j in range(n-1, -1, -1):
+                if i == m-1 and j == n-1:
+                    A[i][j] = max(1, 1 - A[i][j])
+                elif i == m-1:
+                    A[i][j] = max(1, A[i][j+1] - A[i][j])
+                elif j == n-1:
+                    A[i][j] = max(1, A[i+1][j] - A[i][j])
+                else:
+                    A[i][j] = max(1, min(A[i+1][j], A[i][j+1]) - A[i][j])
         
-        return health(A, 0, 0)
+        return A[0][0]
+
+        #------------------------------------------------------------------------------------------------------
+
+        # DP with top down approach
+        # dp = [[-1 for i in range(n)] for j in range(m)]
+
+        # def health(A, i, j):
+        #     if i >= m or j >= n:
+        #         return float('inf')
+        #     if i == m-1 and j == n-1:
+        #         return max(1, 1 - A[i][j])
+        #     if dp[i][j] != -1:
+        #         return dp[i][j]
+        #     dp[i][j] = max(1, min(health(A, i+1, j), health(A, i, j+1)) - A[i][j])
+        #     return dp[i][j]
+        
+        # return health(A, 0, 0)
 # @lc code=end
 
