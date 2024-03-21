37b5ec9d33e916b70a51e15496ebb81151c6ddd6
Irfan <irfansalim77@gmail.com>
2024-03-21 23:09:50 +0530

    Add  M 62.unique-paths.py

--- 62.unique-paths.py
+++ 62.unique-paths.py
@@ -14,14 +14,26 @@ class Solution(object):
         """
         dp = [[-1 for i in range(n)] for j in range(m)]
         
-        def ways(i, j):
-            if i == 0 or j == 0:
-                return 1
-            if dp[i][j] != -1:
-                return dp[i][j]
-            dp[i][j] = ways(i-1, j) + ways(i, j-1)
-            return dp[i][j]
+        for i in range(m):
+            for j in range(n):
+                if i == 0 or j == 0:
+                    dp[i][j] = 1
+                else:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
         
-        return ways(m-1, n-1)
+        return dp[m-1][n-1]
+
+        #------------------------------------------------------------------------------------------------------
+
+        # DP with top down approach
+        # def ways(i, j):
+        #     if i == 0 or j == 0:
+        #         return 1
+        #     if dp[i][j] != -1:
+        #         return dp[i][j]
+        #     dp[i][j] = ways(i-1, j) + ways(i, j-1)
+        #     return dp[i][j]
+        
+        # return ways(m-1, n-1)
 # @lc code=end
 
