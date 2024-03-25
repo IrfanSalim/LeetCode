505ff37643857ca0f06c50245d8791c30e591ed9
Irfan <irfansalim77@gmail.com>
2024-03-25 22:48:58 +0530

    Add  M 152.maximum-product-subarray.py

--- 152.maximum-product-subarray.py
+++ 152.maximum-product-subarray.py
@@ -11,16 +11,39 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        maxSoFar = nums[0]
-        minSoFar = nums[0]
-        maxProduct = nums[0]
-        for i in range(1, len(nums)):
-            curr = nums[i]
-            tempMax = max(curr, curr * maxSoFar, curr * minSoFar)
-            minSoFar = min(curr, curr * maxSoFar, curr * minSoFar)
-            maxSoFar = tempMax
-            maxProduct = max(maxProduct, maxSoFar)
-        return maxProduct
+        # maxSoFar = nums[0]
+        # minSoFar = nums[0]
+        # maxProduct = nums[0]
+        # for i in range(1, len(nums)):
+        #     curr = nums[i]
+        #     tempMax = max(curr, curr * maxSoFar, curr * minSoFar)
+        #     minSoFar = min(curr, curr * maxSoFar, curr * minSoFar)
+        #     maxSoFar = tempMax
+        #     maxProduct = max(maxProduct, maxSoFar)
+        # return maxProduct
+
+        #------------------------------------------------------------------------------------------------------
+
+        # second approach with traversing forward and backward to get max
+        ans = max(nums)
+        product = 1
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                product = 1
+                continue
+            product *= nums[i]
+            ans = max(ans, product)
+
+        product = 1
+        for i in range(len(nums)-1, -1, -1):
+            if nums[i] == 0:
+                product = 1
+                continue
+            product *= nums[i]
+            ans = max(ans, product)
+        
+        return ans
+
 # @lc code=end
     
 
