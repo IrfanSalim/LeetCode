7816c009f97f1b8097a17a51b038b4bc182b0c88
Irfan <irfansalim77@gmail.com>
2024-03-26 22:17:09 +0530

    Add  M 91.decode-ways.py

--- 91.decode-ways.py
+++ 91.decode-ways.py
@@ -11,18 +11,34 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        dp = [0] * (len(s)+1)
-        def decode(i):
-            if i < len(s) and s[i] == "0":
-                return 0
-            if i >= len(s)-1:
-                return 1
-            if dp[i] == 0:
-                dp[i] = decode(i+1)
-                if int(s[i]+s[i+1]) <= 26:
-                    dp[i] += decode(i+2)
-            return dp[i]
-        return decode(0)
+        n = len(s)
+        dp = [0] * (n+1)
+        dp[n] = 1
+        for i in range(n-1, -1, -1):
+            if s[i] == "0":
+                continue
+            else:
+                dp[i] = dp[i+1]
+                if i < n-1 and int(s[i] + s[i+1]) <= 26:
+                    dp[i] += dp[i+2]
+
+        return dp[0]
+
+        #------------------------------------------------------------------------------------------------------
+
+        # top down DP approach
+        # dp = [0] * (len(s)+1)
+        # def decode(i):
+        #     if i < len(s) and s[i] == "0":
+        #         return 0
+        #     if i >= len(s)-1:
+        #         return 1
+        #     if dp[i] == 0:
+        #         dp[i] = decode(i+1)
+        #         if int(s[i]+s[i+1]) <= 26:
+        #             dp[i] += decode(i+2)
+        #     return dp[i]
+        # return decode(0)
         
 # @lc code=end
 
