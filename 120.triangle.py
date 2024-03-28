b95eb6d7b2b1771451f776a2dd6fa54586880029
Irfan <irfansalim77@gmail.com>
2024-03-28 19:35:48 +0530

    Add  M 120.triangle.py

--- 120.triangle.py
+++ 120.triangle.py
@@ -13,18 +13,25 @@ class Solution(object):
         """
         m = len(triangle)
         n = len(triangle[m-1])
-        dp = [[-1] * n for _ in range(m)]
+        for i in range(m-2, -1, -1):
+            for j in range(len(triangle[i])):
+                carry = min(triangle[i+1][j], triangle[i+1][j+1])
+                triangle[i][j] += carry
+        return triangle[0][0]
 
-        def minSum(i, j):
-            if i > m or j > n:
-                return 0
-            if i == m-1:
-                return triangle[i][j]
-            if dp[i][j] == -1:
-                dp[i][j] = min(minSum(i+1, j), minSum(i+1, j+1)) + triangle[i][j]
-            return dp[i][j]
+        # DP top down approach
+        # dp = [[-1] * n for _ in range(m)]
 
-        return minSum(0, 0)
+        # def minSum(i, j):
+        #     if i > m or j > n:
+        #         return 0
+        #     if i == m-1:
+        #         return triangle[i][j]
+        #     if dp[i][j] == -1:
+        #         dp[i][j] = min(minSum(i+1, j), minSum(i+1, j+1)) + triangle[i][j]
+        #     return dp[i][j]
+
+        # return minSum(0, 0)
         
 # @lc code=end
 
