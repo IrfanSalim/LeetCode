cb4bf02fc3d1754507e38f1ac1f83a99561babae
Irfan <irfansalim77@gmail.com>
2024-03-30 15:10:25 +0530

    Add  M 64.minimum-path-sum.py

--- 64.minimum-path-sum.py
+++ 64.minimum-path-sum.py
@@ -13,17 +13,33 @@ class Solution(object):
         """
         m = len(grid)
         n = len(grid[0])
-        dp = [[-1] * n for _ in range(m)]
 
-        def getMin(i, j):
-            if i < 0 or j < 0:
-                return float('inf')
-            if i == 0 and j == 0:
-                return grid[i][j]
-            if dp[i][j] == -1:
-                dp[i][j] = min(getMin(i-1, j), getMin(i, j-1)) + grid[i][j]
-            return dp[i][j]
+        for i in range(1, n):
+            grid[0][i] += grid[0][i-1]
+        
+        for j in range(1, m):
+            grid[j][0] += grid[j-1][0]
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+        
+        return grid[m-1][n-1]
 
-        return getMin(m-1, n-1)
+        #------------------------------------------------------------------------------------------------------
+
+        # top down DP approach
+        # dp = [[-1] * n for _ in range(m)]
+
+        # def getMin(i, j):
+        #     if i < 0 or j < 0:
+        #         return float('inf')
+        #     if i == 0 and j == 0:
+        #         return grid[i][j]
+        #     if dp[i][j] == -1:
+        #         dp[i][j] = min(getMin(i-1, j), getMin(i, j-1)) + grid[i][j]
+        #     return dp[i][j]
+
+        # return getMin(m-1, n-1)
 # @lc code=end
 
