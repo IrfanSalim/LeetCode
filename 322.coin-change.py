79809c12fe7fa940f575dc3a2aac137a47d34942
Irfan <irfansalim77@gmail.com>
2024-03-31 12:01:43 +0530

    Add  M 322.coin-change.py

--- 322.coin-change.py
+++ 322.coin-change.py
@@ -12,20 +12,34 @@ class Solution(object):
         :type amount: int
         :rtype: int
         """
+        if amount == 0:
+            return 0
         n = len(coins)
         dp = [-1] * (amount+1)
+        dp[0] = 0
+        for i in range(1, amount+1):
+            dp[i] = float('inf')
+            for j in range(n):
+                if coins[j] <= i:
+                    dp[i] = min(dp[i], 1+dp[i - coins[j]])
+        return dp[amount] if dp[amount] != float('inf') else -1
 
-        def minCoins(j):
-            if j < 0:
-                return float('inf')
-            if j == 0:
-                return 0
-            if dp[j] == -1:
-                dp[j] = float('inf')
-                for i in range(n):
-                    dp[j] = min(dp[j], 1 + minCoins(j-coins[i]))
-            return dp[j]
-        ans = minCoins(amount) 
-        return ans if ans != float('inf') else -1
+
+        # top down DP approach
+        # n = len(coins)
+        # dp = [-1] * (amount+1)
+
+        # def minCoins(j):
+        #     if j < 0:
+        #         return float('inf')
+        #     if j == 0:
+        #         return 0
+        #     if dp[j] == -1:
+        #         dp[j] = float('inf')
+        #         for i in range(n):
+        #             dp[j] = min(dp[j], 1 + minCoins(j-coins[i]))
+        #     return dp[j]
+        # ans = minCoins(amount) 
+        # return ans if ans != float('inf') else -1
 # @lc code=end
 
