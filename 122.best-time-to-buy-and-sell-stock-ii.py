d26f53288f9e371040ef3e5d79eea9c3c2b0a3a0
Irfan <irfansalim77@gmail.com>
2024-04-02 23:44:22 +0530

    Add ?? 122.best-time-to-buy-and-sell-stock-ii.py

---
+++ 122.best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=122 lang=python
+#
+# [122] Best Time to Buy and Sell Stock II
+#
+
+# @lc code=start
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        ans = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                ans += prices[i] - prices[i-1]
+        return ans
+        
+# @lc code=end
+
