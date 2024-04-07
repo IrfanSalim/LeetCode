184db90fbe3477f4078b2d866e0e472e98c31158
Irfan <irfansalim77@gmail.com>
2024-04-07 14:54:14 +0530

    Add  M 73.set-matrix-zeroes.py

--- 73.set-matrix-zeroes.py
+++ 73.set-matrix-zeroes.py
@@ -12,29 +12,52 @@ class Solution(object):
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
         A = matrix
-        n = len(A)
-        m = len(A[0])
-        for i in range(0,n):
-            flag = 0
-            for j in range(0,m):
+        m = len(A)
+        n = len(A[0])
+        cols = []
+        rows = []
+
+        for i in range(m):
+            for j in range(n):
                 if A[i][j] == 0:
-                    flag = 1
-            if flag == 1:
-                for j in range(0,m):
-                    if A[i][j] != 0:
-                        A[i][j] = "True"
-        for j in range(0,m):
-            flag = 0
-            for i in range(0,n):
-                if A[i][j] == 0:
-                    flag = 1
-            if flag == 1:
-                for i in range(0,n):
-                    if A[i][j] != 0:
-                        A[i][j] = "True"
-        for i in range(0,n):
-            for j in range(0,m):
-                if A[i][j] == "True": A[i][j] = 0
+                    rows.append(i)
+                    cols.append(j)
+        
+        for i in range(len(rows)):
+            for j in range(n):
+                A[rows[i]][j] = 0
+        
+        for i in range(len(cols)):
+            for j in range(m):
+                A[j][cols[i]] = 0
+        
         return A
+
+        #------------------------------------------------------------------------------------------------------
+
+        # n = len(A)
+        # m = len(A[0])
+        # for i in range(0,n):
+        #     flag = 0
+        #     for j in range(0,m):
+        #         if A[i][j] == 0:
+        #             flag = 1
+        #     if flag == 1:
+        #         for j in range(0,m):
+        #             if A[i][j] != 0:
+        #                 A[i][j] = "True"
+        # for j in range(0,m):
+        #     flag = 0
+        #     for i in range(0,n):
+        #         if A[i][j] == 0:
+        #             flag = 1
+        #     if flag == 1:
+        #         for i in range(0,n):
+        #             if A[i][j] != 0:
+        #                 A[i][j] = "True"
+        # for i in range(0,n):
+        #     for j in range(0,m):
+        #         if A[i][j] == "True": A[i][j] = 0
+        # return A
 # @lc code=end
 
