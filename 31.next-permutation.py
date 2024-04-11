d26632c1ad1bb0458a8276defdfeb264aa3bcc83
Irfan <irfansalim77@gmail.com>
2024-04-12 00:26:51 +0530

    Add  M 31.next-permutation.py

--- 31.next-permutation.py
+++ 31.next-permutation.py
@@ -12,22 +12,27 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
         n = len(nums)
-        i = n-2
-        while i >= 0 and nums[i] < nums[i+1]:
+        i = n-1
+
+        while i > 0 and nums[i-1] >= nums[i]:
             i -= 1
+        
         if i == 0:
-            return reversed(nums)
+            nums.reverse()
+            return
+        
+        i -= 1
         
         j = n-1
-        while nums[j] < nums[i]:
+        while j >= 0 and nums[j] <= nums[i]:
             j -= 1
         
         nums[i], nums[j] = nums[j], nums[i]
 
         i, j = i+1, n-1
         while i < j:
-            nums[i], nums[j] = nums[j], nums[i]
-        
-        return nums
+            nums[i], nums[j] = nums[j], nums[i]   
+            i += 1
+            j -= 1     
 # @lc code=end
 
