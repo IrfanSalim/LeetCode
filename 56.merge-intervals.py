ced384b7e011da98fc1711dcb6a5d47242389ac1
Irfan <irfansalim77@gmail.com>
2024-04-15 21:26:13 +0530

    Add ?? 56.merge-intervals.py

---
+++ 56.merge-intervals.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=56 lang=python
+#
+# [56] Merge Intervals
+#
+
+# @lc code=start
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        intervals.sort()
+        n = len(intervals)
+        curr = intervals[0]
+        ans = []
+        for i in range(1, n):
+            if intervals[i][0] <= curr[1]:
+                curr[0] = min(curr[0], intervals[i][0])
+                curr[1] = max(curr[1], intervals[i][1])
+            else:
+                ans.append(curr)
+                curr = intervals[i]
+        ans.append(curr)
+        return ans
+# @lc code=end
+
