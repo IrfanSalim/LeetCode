0701d5122aa5ec45946b9c5e7ab0e87d6222c773
Irfan <irfansalim77@gmail.com>
2024-04-16 08:25:28 +0530

    Add  M 19.remove-nth-node-from-end-of-list.py ?? 42.trapping-rain-water.py

--- 19.remove-nth-node-from-end-of-list.py
+++ 19.remove-nth-node-from-end-of-list.py
@@ -17,28 +17,45 @@ class Solution(object):
         :type n: int
         :rtype: ListNode
         """
-        curr = head
-        size = 0
-
-        #get the size of the linked list
-        while curr:
-            size += 1
+        # approach with a single pass
+        dummy = ListNode(0, head)
+        curr = temp = dummy
+        # move the curr ahead n times
+        for i in range(n):
             curr = curr.next
-
-        #edge case   
-        if size == 1 and n > 0:
-            return None
-        if n >= size:
-            head = head.next
-            return head
-        
-        curr = head
-        #iterate to the node prior to the nth node from end
-        for _ in range(size - n - 1):
+            
+        # move temp and head together until curr reaches end
+        while curr.next:
             curr = curr.next
+            temp = temp.next
         
-        #delete the nth node by removing the connection
-        curr.next = curr.next.next
-        return head
+        #remove nth node
+        temp.next = temp.next.next
+        return dummy.next
+
+        # this approach required two passes on the LL
+        # curr = head
+        # size = 0
+
+        # #get the size of the linked list
+        # while curr:
+        #     size += 1
+        #     curr = curr.next
+
+        # #edge case   
+        # if size == 1 and n > 0:
+        #     return None
+        # if n >= size:
+        #     head = head.next
+        #     return head
+        
+        # curr = head
+        # #iterate to the node prior to the nth node from end
+        # for _ in range(size - n - 1):
+        #     curr = curr.next
+        
+        # #delete the nth node by removing the connection
+        # curr.next = curr.next.next
+        # return head
 # @lc code=end
 
