4e76ae78c909d485cc1cbdf8ce2cdd8a62b341d6
Irfan <irfansalim77@gmail.com>
2024-04-19 19:01:05 +0530

    Add  M 137.single-number-ii.py

--- 137.single-number-ii.py
+++ 137.single-number-ii.py
@@ -12,9 +12,34 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        count = Counter(nums)
-        for key in count:
-            if count[key] == 1:
-                return key
+        # bit manipulation approach
+        # ones is a bitmask to represent the ith bit had appeared once
+        ones = 0
+        # twos is a bitmask to represent the ith bit had appeared twice
+        twos = 0
+        common_mask = 0
+        for a in nums:
+            twos |= ones & a
+            ones ^= a
+            common_mask = ~(ones & twos)
+            ones &= common_mask
+            twos &= common_mask
+        return ones
+
+        # ans = 0
+        # for i in range(32):
+        #     count = 0
+        #     for num in nums:
+        #         if (num & (1 << i)) > 0:
+        #             count += 1
+        #     if count % 3 == 1:
+        #         ans |= (1 << i)
+        # return ans
+
+        # brute force approach
+        # count = Counter(nums)
+        # for key in count:
+        #     if count[key] == 1:
+        #         return key
 # @lc code=end
 
