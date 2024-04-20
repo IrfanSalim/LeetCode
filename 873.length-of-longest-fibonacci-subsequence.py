7833b14b14d15b9312af8c740b1b4a8246e85248
Irfan <irfansalim77@gmail.com>
2024-04-20 12:27:21 +0530

    Add  M 873.length-of-longest-fibonacci-subsequence.py

--- 873.length-of-longest-fibonacci-subsequence.py
+++ 873.length-of-longest-fibonacci-subsequence.py
@@ -11,15 +11,30 @@ class Solution(object):
         :type arr: List[int]
         :rtype: int
         """
+        # optimized dp solution
         A = arr
         n = len(A)
-        dp = [[2 for _ in range(n+1)] for _ in range(n+1)]
+        dp = [[2] * (n) for _ in range(n)]
+        indexes = {x: i for i,x in enumerate(A)}
         for j in range(n):
             for i in range(j):
-                for k in range(i):
-                    if A[k]+A[i] == A[j]:
-                        dp[i][j] = max(dp[i][j], 1+dp[k][i])
+                req = A[j] - A[i]
+                if req in indexes and indexes[req] < i:
+                    k = indexes[req]
+                    dp[i][j] = max(dp[i][j], 1 + dp[k][i])
         ans = max([max(n) for n in dp])
-        return 0 if ans <= 2 else ans
+        return ans if ans > 2 else 0
+
+        # brute force solution with n^3 TC
+        # A = arr
+        # n = len(A)
+        # dp = [[2 for _ in range(n+1)] for _ in range(n+1)]
+        # for j in range(n):
+        #     for i in range(j):
+        #         for k in range(i):
+        #             if A[k]+A[i] == A[j]:
+        #                 dp[i][j] = max(dp[i][j], 1+dp[k][i])
+        # ans = max([max(n) for n in dp])
+        # return 0 if ans <= 2 else ans
 # @lc code=end
 
