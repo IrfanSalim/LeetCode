06bfee09972fcb166c0e00dc8357a94a922e64f7
Irfan <irfansalim77@gmail.com>
2024-04-27 23:24:09 +0530

    Add ?? 1391.check-if-there-is-a-valid-path-in-a-grid.py

---
+++ 1391.check-if-there-is-a-valid-path-in-a-grid.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=1391 lang=python
+#
+# [1391] Check if There is a Valid Path in a Grid
+#
+
+# @lc code=start
+class Solution(object):
+    def hasValidPath(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: bool
+        """
+        
+# @lc code=end
+
