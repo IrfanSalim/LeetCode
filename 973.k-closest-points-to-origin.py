bcc4f6be84800c642a07dd0d7a568865b5dadbbf
Irfan <irfansalim77@gmail.com>
2024-04-30 00:50:08 +0530

    Add  M 973.k-closest-points-to-origin.py

--- 973.k-closest-points-to-origin.py
+++ 973.k-closest-points-to-origin.py
@@ -13,18 +13,25 @@ class Solution(object):
         :type k: int
         :rtype: List[List[int]]
         """
-        heap = []
-        for i in range(k):
-            dist = points[i][0]**2 + points[i][1]**2
-            heapq.heappush(heap, (-dist, points[i][0], points[i][1]))
+        # sorting solution
+        if k == len(points):
+            return points
+        points = sorted(points, key = lambda points: points[0] ** 2 + points[1] ** 2)
+        return points[:k]
+    
+        # max heap solution
+        # heap = []
+        # for i in range(k):
+        #     dist = points[i][0]**2 + points[i][1]**2
+        #     heapq.heappush(heap, (-dist, points[i][0], points[i][1]))
         
-        for j in range(k, len(points)):
-            dist = points[j][0]**2 + points[j][1]**2
-            if dist < -heap[0][0]:
-                heapq.heappush(heap, (-dist, points[j][0], points[j][1]))
-                heapq.heappop(heap)
+        # for j in range(k, len(points)):
+        #     dist = points[j][0]**2 + points[j][1]**2
+        #     if dist < -heap[0][0]:
+        #         heapq.heappush(heap, (-dist, points[j][0], points[j][1]))
+        #         heapq.heappop(heap)
         
-        heap = [[point[1], point[2]] for point in heap]
-        return heap
+        # heap = [[point[1], point[2]] for point in heap]
+        # return heap
 # @lc code=end
 
