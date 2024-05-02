206907ba66c45dbc5d3b179d28c4a434855243dd
Irfan <irfansalim77@gmail.com>
2024-05-02 07:20:39 +0530

    Add  M 1143.longest-common-subsequence.py

--- 1143.longest-common-subsequence.py
+++ 1143.longest-common-subsequence.py
@@ -12,20 +12,35 @@ class Solution(object):
         :type text2: str
         :rtype: int
         """
+        # bottom up dp approach
         m = len(text1)
         n = len(text2)
-        dp = [[-1 for i in range(n+1)] for j in range(m+1)]
-        
-        def helper(i, j):
-            if i == 0 or j == 0:
-                return 0
-            if dp[i][j] == -1:
-                if text1[i-1] == text2[j-1]:
-                    dp[i][j] = helper(i - 1, j - 1) + 1
-                else:
-                    dp[i][j] = max(helper(i-1, j), helper(i, j-1))
-            return dp[i][j]
+        dp = [[0 for i in range(n+1)] for j in range(m+1)]
 
-        return helper(m, n)
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        
+        return dp[m][n]
+
+        # top down DP approach
+        # m = len(text1)
+        # n = len(text2)
+        # dp = [[-1 for i in range(n+1)] for j in range(m+1)]
+
+        # def helper(i, j):
+        #     if i == 0 or j == 0:
+        #         return 0
+        #     if dp[i][j] == -1:
+        #         if text1[i-1] == text2[j-1]:
+        #             dp[i][j] = helper(i - 1, j - 1) + 1
+        #         else:
+        #             dp[i][j] = max(helper(i-1, j), helper(i, j-1))
+        #     return dp[i][j]
+
+        # return helper(m, n)
 # @lc code=end
 
