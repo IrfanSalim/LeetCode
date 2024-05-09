7d2e7551d23ec0af6efa1b33f05a9a2fd894a09e
Irfan <irfansalim77@gmail.com>
2024-05-09 19:26:12 +0530

    Add  M 24.swap-nodes-in-pairs.py

--- 24.swap-nodes-in-pairs.py
+++ 24.swap-nodes-in-pairs.py
@@ -6,16 +6,26 @@
 
 # @lc code=start
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution(object):
     def swapPairs(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
         """
-        
+        dummy = ListNode()
+        dummy.next = head
+        curr = dummy
+        while curr.next and curr.next.next:
+            first = curr.next
+            second = first.next
+            curr.next = second
+            first.next = second.next
+            second.next = first
+            curr = curr.next.next
+        return dummy.next
 # @lc code=end
 
