4e0a7a2b3641773fb1ed38667077c0574e0a96d2
Irfan <irfansalim77@gmail.com>
2024-08-03 10:24:33 +0530

    Add  M 105.construct-binary-tree-from-preorder-and-inorder-traversal.py

--- 105.construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105.construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -12,20 +12,45 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
-    def buildTree(self, A, B):
-        if not A or not B:
-            return None
-       
-        root_val = A[0]
-        root = TreeNode(root_val)
-       
-        # find the index of the root in the inorder traversal
-        root_idx = B.index(root_val)
-       
-        # recursively construct the left and right subtrees
-        root.left = self.buildTree(A[1:root_idx+1], B[:root_idx])
-        root.right = self.buildTree(A[root_idx+1:], B[root_idx+1:])
-       
-        return root
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+        inMap = {val: i for i, val in enumerate(inorder)}
+        
+        def build(preorder, psi, pei, inorder, isi, iei):
+            if psi > pei or isi > iei:
+                return None
+            if psi == pei:
+                return TreeNode(preorder[psi])
+            idx = inMap[preorder[psi]]
+
+            root = TreeNode(preorder[psi])
+            root.left = build(preorder, psi+1, psi+(idx-isi), inorder, isi, idx-1)
+            root.right = build(preorder, psi+(idx-isi) + 1, pei, inorder, idx+1, iei)
+
+            return root
+        
+        n = len(preorder)
+        return build(preorder, 0, n-1, inorder, 0, n-1)
+        
+
+    # def buildTree(self, A, B):
+    #     if not A or not B:
+    #         return None
+
+    #     root_val = A[0]
+    #     root = TreeNode(root_val)
+
+    #     # find the index of the root in the inorder traversal
+    #     root_idx = B.index(root_val)
+
+    #     # recursively construct the left and right subtrees
+    #     root.left = self.buildTree(A[1:root_idx+1], B[:root_idx])
+    #     root.right = self.buildTree(A[root_idx+1:], B[root_idx+1:])
+
+    #     return root
 # @lc code=end
 
